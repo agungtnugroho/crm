f53eff09b8c9e336281dfcb0be0d000babcaf671
Agung Nugroho <agungteguhnugroho@gmail.com>
2013-09-27 14:24:33 +0700

    Add user model with Devise

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,7 @@ Crm::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # required for devise in heroku
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
